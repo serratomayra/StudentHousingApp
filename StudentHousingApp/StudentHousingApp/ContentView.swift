a94cafb346b7297b9d61ac95af6f7f195809ff3f
mayraserrato <serratomayra@outlook.com>
2020-02-03 14:42:23 -0700

    Testing code
    - Added " ! " to the text "First View!"

--- StudentHousingApp/StudentHousingApp/ContentView.swift
+++ StudentHousingApp/StudentHousingApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
  
     var body: some View {
         TabView(selection: $selection){
-            Text("First View")
+            Text("First View!")
                 .font(.title)
                 .tabItem {
                     VStack {

877ac6f36c9ce5ab9d71227e7730c61dfcb6792c
mayraserrato <serratomayra@outlook.com>
2020-02-10 02:32:31 -0700

    LoginScreen features
    - Added code snippets for features to add to UI

--- StudentHousingApp/StudentHousingApp/LoginScreen.swift
+++ StudentHousingApp/StudentHousingApp/LoginScreen.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class LoginScreen: UIView {
 
+    @IBInspectable var shadowColor:UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: CGFloat = 0 {
+        didSet {
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+    
+    @IBInspectable var shadowOffsetY: CGFloat = 0 {
+        didSet {
+            layer.shadowOffset.height = shadowOffsetY
+        }
+    }
+    
     @IBOutlet var contentView: UIView!
     @IBOutlet weak var mainLabel: UILabel!
     
